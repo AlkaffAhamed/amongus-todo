4057524c3eae76c8f0b37e710349499fc87cf296
AlkaffAhamed <alkaffahamed@hotmail.com>
2021-09-15 17:08:22 +0800

    Modified index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('UP AND RUNNING @', port);
+  console.log('JUST ANOTHER LINE!');
+  console.log('JUST ANOTHER LINE!');
 });
